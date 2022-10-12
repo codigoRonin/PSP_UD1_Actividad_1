83daac7825ec0dd1c2920f5bb1b232867595e8dd
SGalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-12 11:52:16 +0200

    commit del ejercicio 1

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,4 +1,29 @@
+import java.io.InputStream;
+
 public class Ejercicio1 {
+
+    /*Crea un programa que invoque al comando Tree sobre la carpeta del proyecto y muestra su salida.*/
     public static void main(String[] args) {
+
+        ProcessBuilder processBuilder = new ProcessBuilder("cmd","/C","tree");
+        Process process;
+
+        try {
+
+            int caracterUnicode;
+            
+            process = processBuilder.start();
+            InputStream inputStream = process.getInputStream();
+
+            while ((caracterUnicode = inputStream.read()) != -1) {
+                
+                System.out.print((char)caracterUnicode);
+                
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
     }
 }
