3890a4bc247a03cb890014035e5d23dbde0dd256
SGalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-12 14:44:59 +0200

    commit del ejercicio 2

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -4,8 +4,15 @@ public class Ejercicio1 {
 
     /*Crea un programa que invoque al comando Tree sobre la carpeta del proyecto y muestra su salida.*/
     public static void main(String[] args) {
+       
+        String ruta = args.length== 0 ? "..\\" : args[0];//si array vacio asignamos ruta defecto, sino la que viene por parametros
 
-        ProcessBuilder processBuilder = new ProcessBuilder("cmd","/C","tree");
+        // String ruta = args[0];
+        // if(ruta == null){
+        //     ruta = "..\\";
+        // }
+        
+        ProcessBuilder processBuilder = new ProcessBuilder("cmd","/C","tree",ruta);
         Process process;
 
         try {
--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,4 +1,33 @@
+import java.io.File;
+import java.io.InputStream;
+
 public class Ejercicio2 {
+
+    /*Partiendo del código anterior,
+    crea un programa que muestre el árbol de carpetas de un directorio recibido como parámetro por la entrada estándar. */
+
     public static void main(String[] args) {
+        Process proceso;
+        File file = new File(".\\bin");
+
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "Ejercicio1", "..\\");
+        processBuilder.directory(file);
+
+        try {
+            int caracterUnicode;
+            proceso = processBuilder.start();
+
+            InputStream inputStream = proceso.getInputStream(); 
+
+            while ((caracterUnicode = inputStream.read())!= -1) {
+
+                System.out.print((char)caracterUnicode);
+                
+            }
+            
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
     }
 }
