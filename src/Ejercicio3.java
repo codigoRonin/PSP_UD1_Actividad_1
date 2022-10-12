9eac2be6c319e054849affd6418c264dd9b56fb1
SGalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-12 19:37:43 +0200

    commit ejercicios 3 y 4

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,4 +1,32 @@
 public class Ejercicio3 {
+    /*
+     * Crea un programa sencillo que calcule el factorial de un número recibido como
+     * parámetro del método main.(Utiliza Recursividad)
+     */
     public static void main(String[] args) {
+
+        int numero = args.length == 0 ? 7 : Integer.parseInt(args[0]);
+         
+        System.out.println("el resultado es: " + calculoFactorial(numero, 1, 1));
+        
+        //int resultado = 1;
+        // for (int i = 1; i <= numero; i++) {
+        // resultado*=i;
+        // System.out.println(i +" : "+ resultado);
+        // }
+        // System.out.println("el resultado es: "+resultado);
+  
     }
+
+    public static int calculoFactorial(int numeroARGS, int resultado,int contador){
+        
+        resultado*=contador;
+        //System.out.println(contador +" : "+ resultado);
+        if(contador++ < numeroARGS){
+            return calculoFactorial(numeroARGS,resultado,contador);
+        }
+                
+        return resultado;
+    }
+
 }
--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -1,4 +1,8 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
+
+        /*Crea un programa que, ejecutando Ejercicio3, nos calcule el cuadrado del factorial de un número dado por teclado.*/
+
+        
     }
 }
