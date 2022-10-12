c8785b384c7a00edbf0b2324fbfa07b3686b820c
SGalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-12 19:58:17 +0200

    prueba push

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -3,6 +3,8 @@ public class Ejercicio4 {
 
         /*Crea un programa que, ejecutando Ejercicio3, nos calcule el cuadrado del factorial de un número dado por teclado.*/
 
+
+        System.out.println("probando github");
         
     }
 }
