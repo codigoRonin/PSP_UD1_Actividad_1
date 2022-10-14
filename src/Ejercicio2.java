6df1349e45c9e9f17b5a7f1208257050a438c7d7
Leonel Lopez <leonel.lopez.santos@iestubalcain.net>
2022-10-14 13:25:49 +0200

    Ejercicio 2 acabado

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,4 +1,48 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
+
+        ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "tree");
+
+        Scanner stdin = new Scanner(System.in);
+        System.out.print("Introduzca la ruta: ");
+        String ruta = stdin.next();
+        File file = new File(ruta);
+
+        processBuilder.directory(file);
+        stdin.close();
+        try{
+            Process process = processBuilder.start();
+
+            InputStream inputStream = process.getInputStream();
+            int byteInputStream;
+            while((byteInputStream = inputStream.read()) != -1){
+                System.out.print((char) byteInputStream);
+            }
+            inputStream.close();
+
+            if(process.waitFor() != 0) {
+                InputStream inputStreamErr = process.getErrorStream();
+                BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStreamErr)
+                );
+                String lineaInputStreamErr;
+                while((lineaInputStreamErr = bufferedReader.readLine()) != null) {
+                    System.out.println("Error ->: " + lineaInputStreamErr);
+                }
+                inputStreamErr.close();
+            }
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
+        }
     }
 }
