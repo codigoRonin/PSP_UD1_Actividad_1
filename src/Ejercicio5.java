7fcea23870856bb533584365a1dd6ae30447d025
Cora <cora.gonzalez.perez@iestubalcain.net>
2022-10-16 15:04:40 +0200

    Solucion ejercicio 5 bueno

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,6 +1,10 @@
 public class Ejercicio5 {
     public static void main(String[] args) {
 
+        if(args.length != 1){
+            System.exit(-1);
+        }
+
         String nombre = args[0];
         System.out.println("El nombre es: " + nombre);
 
