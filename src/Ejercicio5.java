592c02519309f968742322f9641e9f26f499e819
Michel <codigoronin@gmail.com>
2022-10-28 19:09:55 +0200

    Upload All Exercices

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,5 +1,11 @@
 public class Ejercicio5 {
     public static void main(String[] args) {
 
+        if (args.length < 1)
+            System.exit(-1);
+       
+        System.out.println(args[0]);
+        System.exit(0);
     }
+
 }
