160af4922b537e14d055f4be6652509dd8778346
Leonel Lopez <leonel.lopez.santos@iestubalcain.net>
2022-10-30 23:15:51 +0100

    Correccion ejercicio 5

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -1,6 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Ejercicio4 {
@@ -17,9 +19,13 @@ public class Ejercicio4 {
         try{
             Process process = processBuilder.start();
             InputStream inputStream = process.getInputStream();
-            int c;
-            while ((c = inputStream.read()) != -1) {
-                System.out.print((char) c);
+            BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream)
+            );
+            String linea;
+            while((linea = bufferedReader.readLine()) != null) {
+                int numero = Integer.parseInt(linea);
+                System.out.println(numero*numero);
             }
             inputStream.close();
         } catch (IOException ioException) {
