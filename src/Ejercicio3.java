e418998e4cc472243271d6311bb47eb214be1c31
Leonel Lopez <leonel.lopez.santos@iestubalcain.net>
2022-10-30 22:17:18 +0100

    Ejercicios 3 y 4

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,4 +1,28 @@
+
 public class Ejercicio3 {
     public static void main(String[] args) {
+
+        if ((args.length != 0) && isInt(args[0]) ) {
+            int num = Integer.parseInt(args[0]);
+            System.out.println(factorial(num));
+        }
+
+    }
+
+    public static int factorial (int numero) {
+        if (numero == 0) {
+            return 1;
+        }
+            
+        return (numero*factorial(numero-1));
+    }
+
+    public static boolean isInt (String string) {
+        try{
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException numberFormatException) {
+            return false;
+        }
     }
 }
--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -1,4 +1,42 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Ejercicio4 {
+    private static final String RUTA = "./bin";
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = obtenerNumero(scanner);
+        
+        String string = String.valueOf(num);
+        File directorio = new File(RUTA);
+
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "Ejercicio3", string);
+        processBuilder.directory(directorio);
+        try{
+            Process process = processBuilder.start();
+            InputStream inputStream = process.getInputStream();
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                System.out.print((char) c);
+            }
+            inputStream.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+
+
+    public static int obtenerNumero(Scanner scanner) {
+        System.out.println("Introduzca un número: ");
+        try {
+            int num = scanner.nextInt();
+            return num;
+        } catch (NumberFormatException exNumberFormatException) {
+            System.out.println("Debe introducir un número.");
+            return obtenerNumero(scanner);
+        }
     }
 }
